a6196fb5f54ae9e493440118edf8cd442795874c
Mahdyar Karimi <rhlkr7474@gmail.com>
2024-09-17 05:04:07 +0000

    etag example

--- s3/s3-object/etags/main.tf
+++ s3/s3-object/etags/main.tf
@@ -8,6 +8,7 @@ terraform {
 }
 
 provider "aws" {
+  region = "ap-northeast-1"  # Set the AWS region to Tokyo (ap-northeast-1)
 }
 
 resource "aws_s3_bucket" "default" {
@@ -18,6 +19,6 @@ resource "aws_s3_object" "object" {
   key    = "myfile.txt"
   source = "myfile.txt"
 
-  # necessary for terraform notice change in document content
+  # Necessary for Terraform to detect changes in the file content
   etag = filemd5("myfile.txt")
 }
